dff1fad6e92f24866cf67e4f1c23bdcd4c38c1da
Ofonime udo <unwanaofon001@gmail.com>
2026-09-17 17:13:59 +0100

    tracker

--- expence_tracker.py
+++ expence_tracker.py
@@ -4,19 +4,15 @@
 # Create a list and store the expense category
 # Select the category of expenses from the list 
 # Create a while loop for the category selection
-def expense_track():
+
+import csv
+
+
+def add_expense(expense):
 
     print("Welcome to expenses tracker")
 
-    expense_category = {
-        "🍲Food": [],
-        "🌆Work": [],
-        "🤑Utilities": [],
-        "🏠Rent": [],
-        "🪁Fun": [],
-        "📳Misceleneous": []
-    }
-
+    
 
 
     while True:
@@ -25,36 +21,193 @@ def expense_track():
         
 
         if add_expense == "yes":
-            product_input = input("Enter your expense: ")
+            product_input = input("Enter your description: ")
             amount_input = float(input("Enter the amount: $"))
 
+            expense_category = [
+                    "🍲Food",
+                    "🌆Work",
+                    "🤑Utilities",
+                    "🏠Rent",
+                    "🪁Fun",
+                    "📳Misceleneous",
+                ]
+            
                            
             for i, value in enumerate(expense_category):
-             print(f"{i + 1}. {value}")
+              print(f"{i + 1}. {value}")
 
+                     
+            value = int(input("Select category: ")) - 1
+
+            if value < 0 or value >= len(expense_category):
+                    print("Enter numer from 1-6")
+                    continue
+
+        
+
+            expenses = {
+                "category": expense_category[value],
+                "description": product_input,
+                "amount": amount_input
+               
                 
-            value = int(input("Select category: ")) -1
+                                                }
+            expense.append(expenses)
             
-            print(f"You have added ({product_input} = {amount_input}) to your expense list")
+            
+            print(f"Expenses successfully added! {expense}")
             
            
         elif add_expense == "no":
             print("That will be all for now, thank you for using this tracker")
             break 
         else:
-            print('Invalid choice, Enter yes/no')
+            print('Invalid choice, Enter a valid choice yes/no')
+
+           
+            
+
+expense = [] 
 
 
-expense_track()  
+add_expense(expense)
 
-def expense_table(product_input, amount_input):
 
-    total_amount = sum(amount_input)
-    display = {
-        "category"
-    }
+
+def save_expenses(expense):
+
+
+    with open("expense.csv", "w", newline="", encoding="utf-8") as file:
+
+        sectionames = ["category", "description", "amount"]
+
+        writer = csv.DictWriter(file, fieldnames=sectionames)
+
+        writer.writeheader()
+
+        writer.writerows(expense)
+
+save_expenses(expense)
+
+
+def load_expenses():
+    # df = pd.read_csv('expense.csv', dtype={'amount': int, 'category': str, 'description': str})
+
+     #expenses = []
+     expenses = load_expenses()
+ 
+
+     add_expense(expense)
+
+     save_expenses(expense)
+
+     
+     with open("expense.csv", "r", newline="", encoding="utf-8") as file:
+         reader = csv.DictReader(file)
+
+         for row in reader:
+            row["amount"] = float(row["amount"])
+                   
+
+            expenses.append(row)
+
+
+        
+     return expenses 
+
+
+def view_expenses(expense):
+
+     for i, store in enumerate(expense):
+
+
+
+        print(f"{i + 1}. {store}")
     
-    print(f"{product_input} = {amount_input}")
+
+view_expenses(expense)   
+
+
+def total_expense(expense):
+
+    total = 0
+
+    for i in expense:
+
+      
+         total += i["amount"]
+    return total  
+   
+print(f"Total Expenditure = {total_expense(expense)}")  
+
+
+def spend_by_category(expense):
+
+    sort_category = {}
+
+    for i in expense:
+        sort_category += expense["category"]
+        if expense["category"] == sort_category:
+            
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+        
+
+
+
+
+         
+   
+
+
+
+
+
+
+
+   
+
+
+        
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+# def delete_expense():
+
+
+        
+
+
+
+
     
     
 
